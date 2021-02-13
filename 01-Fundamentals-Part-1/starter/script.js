50e8e6d1c78fa3648fa725d3ebb77def4be49b57
JC <johnchristopherllaguno1@gmail.com>
2021-02-13 12:34:21 +0800

    values_and_variables

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,47 @@
+// Value = Smallest unit of information in JavaScript
+let js = 'amazing';
+// if (js === 'amazing') alert('JavaScript is FUN!');
+// 40 + 8 + 23 - 10;
+
+// Displaying literal values in the console
+console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+// Declaring a variable
+let firstName = "Matilda";
+
+// Variable naming convention in JavaScript (Camelcase Notation)
+// let firstNamePerson
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// Note:
+// Variables can be considered as a box which can store a value
+// Variable names cannot start with a number
+// Variable names can only contain numbers, letters, underscores or the dollar sign 
+// Avoid reserved keywords
+// Do not use uppercase letters in the beginning of variable names 
+// Variables that are all in uppercase are for constants
+// Variable names should be descriptive
+
+let jonas_matilda = "JM";
+let $function = 27;
+let name = "Jonas"; // "name" is a reserved keyword, avoid using it
+
+let person = "jonas";
+let PI = 3.1415;
+
+let myFirstJob = "Coder";
+let myCurrentJob = "Teacher";
+
+// not advisable
+let job1 = "programmer";
+let job2 = "teacher";
+
+console.log(myFirstJob);
+
+
---
+++ 01-Fundamentals-Part-1/test/js.js
@@ -0,0 +1,16 @@
+const passwords = [
+    "123456",
+    "password",
+    "admin",
+    "freecodecamp",
+    "mypassword123",
+];
+
+let passwordLength = passwords.filter(pass => pass.length >= 9);
+console.log(passwordLength);
+
+let js = 'fun';
+
+if (js === 'fun') {
+    alert('I will be successful!');
+}
