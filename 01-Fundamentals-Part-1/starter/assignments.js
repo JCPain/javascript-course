6cfe19567d593748e25ea08865e8ceb676dc5782
JC <johnchristopherllaguno1@gmail.com>
2021-02-15 13:17:03 +0800

    let const and var

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,8 @@
+// Values and Variables
+let country = "Philippines";
+let continent = "Asia";
+let population = "110,800,000";
+
+console.log("Country: " + country);
+console.log("Continent: " + continent);
+console.log("Population: " + population);
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,5 @@
+/*
+// Values and Variables
 // Value = Smallest unit of information in JavaScript
 let js = 'amazing';
 // if (js === 'amazing') alert('JavaScript is FUN!');
@@ -22,12 +24,13 @@ console.log(firstName);
 // Note:
 // Variables can be considered as a box which can store a value
 // Variable names cannot start with a number
-// Variable names can only contain numbers, letters, underscores or the dollar sign 
+// Variable names can only contain numbers, letters, underscores or the dollar sign
 // Avoid reserved keywords
-// Do not use uppercase letters in the beginning of variable names 
+// Do not use uppercase letters in the beginning of variable names
 // Variables that are all in uppercase are for constants
 // Variable names should be descriptive
 
+// Varaible name conventions
 let jonas_matilda = "JM";
 let $function = 27;
 let name = "Jonas"; // "name" is a reserved keyword, avoid using it
@@ -43,5 +46,38 @@ let job1 = "programmer";
 let job2 = "teacher";
 
 console.log(myFirstJob);
+//-------------------------------------------------------------------------------------------------------------------
+// Data Types
+let javascriptIsFun = true;
+console.log(javascriptIsFun); // true
 
+// console.log(typeof true);
+console.log(typeof javascriptIsFun); // boolean
+// console.log(typeof 23);
+// console.log(typeof 'Jonas');
 
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun); // string
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
+*/
+// let, const and var
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990;
+// const job;
+
+var job = 'programmer';
+job = 'teacher';
+
+lastName = 'Schmedtmann';
+console.log(lastName);
